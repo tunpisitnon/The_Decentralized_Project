36fb82e6dde88638f4b26e646cbfef6594dcca22
thanlnwza <thanyabun.ph@gmail.com>
2023-03-21 19:37:02 +0700

    update route

--- backend/run.py
+++ backend/run.py
@@ -3,16 +3,36 @@ from backend.contract.Contract_Coin import Coin_Contract
 
 from backend.app.app import app
 
+from flask import jsonify, request
+
 wood_contract = Wood_Contract()
 coin_contract = Coin_Contract()
 
 
 @app.route('/')
 def hello_world():
-    wood_hello = wood_contract.hello()
-    coin_hello = coin_contract.hello()
+    return jsonify({
+        'status_code': 200,
+        'status': 'success',
+    })
 
-    return f'{wood_hello} | {coin_hello}'
+
+@app.route('/wood/total_supply', methods=['GET', 'POST'])
+def wood():
+    return jsonify({'total wood supply': wood_contract.get_total_supply()})
+
+
+@app.route('/wood/supply_left', methods=['GET', 'POST'])
+def wood_supply_left():
+    return jsonify({'wood supply left': wood_contract.wood_supply_left()})
+
+
+@app.route('/wood/balance', methods=['POST'])
+def wood_balance():
+    _address = request.get_json()['address']
+    return jsonify({
+        'wood balance': wood_contract.get_balance(_address)
+    })
 
 
 if __name__ == '__main__':
