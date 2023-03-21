2a80e8c4a0f6f06923c572aedcc9e27e72996268
thanlnwza <thanyabun.ph@gmail.com>
2023-03-21 19:37:24 +0700

    U: essential function

--- backend/contract/Contract_Wood.py
+++ backend/contract/Contract_Wood.py
@@ -6,6 +6,13 @@ class Wood_Contract:
 
     def __init__(self):
         self.wood_contract = web3.eth.contract(address=config.Tree_address, abi=config.Tree_abi)
+        self.owner_wallet = "0x6AAEd57e79C25F5552DAe735f32e06D7bb7925c6"
 
-    def hello(self):
-        return 'yes? from Wood_Contract'
+    def get_total_supply(self):
+        return self.wood_contract.functions.totalSupply().call()
+
+    def get_balance(self, address):
+        return self.wood_contract.functions.balanceOf(address).call()
+
+    def wood_supply_left(self):
+        return self.wood_contract.functions.balanceOf(self.owner_wallet).call()
