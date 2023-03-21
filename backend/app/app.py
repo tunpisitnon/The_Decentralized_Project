34f7565647e244b10b8a16ed7f4ff2406a4e3666
thanlnwza <thanyabun.ph@gmail.com>
2023-03-22 03:46:29 +0700

    U: delete comment

--- backend/app/app.py
+++ backend/app/app.py
@@ -8,18 +8,3 @@ app.config.from_object(config)
 
 web3 = Web3(Web3.HTTPProvider(config.GANACHE_URL))
 web3.eth.defaultAccount = web3.eth.accounts[0]
-
-
-# accounts = web3.eth.accounts
-# address = "0x08347F4c07ae85E4A9ab31a02e962993a9a55930"
-# print(accounts)
-#
-# with open('../contract_abi.json', 'r') as f:
-#     abi = json.load(f)
-#
-# my_contract = web3.eth.contract(address=address, abi=abi)
-#
-# tx_hash = my_contract.functions.transfer(accounts[1], 100).transact({'from': accounts[0]})
-# print(tx_hash.hex())
-#
-# tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
